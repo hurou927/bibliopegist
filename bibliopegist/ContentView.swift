489eddf585278605a8c412211d8070326bf09fb3
shunji.funasaka <shunji.funasaka@linecorp.com>
2020-07-14 21:38:06 +0900

    Initial Commit

---
+++ bibliopegist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bibliopegist
+//
+//  Created by JC20079 on 2020/07/14.
+//  Copyright © 2020 hurou927. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
